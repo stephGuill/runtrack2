04862d6eee7e9dd13503f9ebd657d82b322c421a
stephGuill <stephane.guillamo@laplateforme.io>
2025-09-26 14:42:24 +0200

    fini job 02, jour 02

--- Jour 02/job 02/index.php
+++ Jour 02/job 02/index.php
@@ -4,9 +4,18 @@
 // Liste des nombres interdits
 $exclus = [26, 37, 88, 1111, 3233];
 
+// Fonction manuelle pour remplacer in_array
+function manualInArray($needle, $haystack) {
+    for ($i = 0; $i < count($haystack); $i++) {
+        if ($haystack[$i] === $needle) {
+            return true;
+        }
+    }
+    return false;
+}
 for ($i = 0; $i <= 1337; $i++) {
-    // On affiche uniquement si $i n’est pas dans la liste des exclus
-    if (!in_array($i, $exclus)) {
+    // On affiche uniquement si $i n'est pas dans la liste des exclus
+    if (!manualInArray($i, $exclus)) {
         echo $i . "<br />";
     }
 }
